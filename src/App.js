b77bd5e0b56977dc911c5b559de03a9bc7ba312d
Ken Thore <kenthore@live.no>
2023-09-19 21:49:22 +0200

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Route, Routes, Link, useParams } from "react-router-dom";
+import "./App.css";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function ProductPage() {
+  return <div>Product Page</div>;
+}
+
+function CheckOutPage() {
+  return <div>Checkout Page</div>;
+}
+
+function CheckOutSuccessPage() {
+  return <div>Checkout Success Page</div>;
+}
+
+function ContactPage() {
+  return <div>Contact Page</div>;
+}
+
+function NotFound() {
+  return <div>Not found</div>;
+}
+
+function Cart() {
+  return <div>Cart</div>;
+}
+
+function Header() {
+  return (
+    <header>
+      <div>Header</div>
+      <Nav />
+      <Cart />
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      Nav
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/product">Product</Link>
+        </li>
+        <li>
+          <Link to="/checkout">Checkout</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function Footer() {
+  return <footer>Footer</footer>;
+}
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="product" element={<ProductPage />} />
+          <Route path="checkout" element={<CheckOutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
